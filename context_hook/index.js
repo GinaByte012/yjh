187216f3c9de731c83a3399c88442ddf7393b9b9
jayN-ohio <bjeollllay@gmail.com>
2022-11-06 00:57:00 +0900

    v0.01, first commit history

---
+++ context_hook/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ContextHook from './ContextHook';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <ContextHook />
+);
